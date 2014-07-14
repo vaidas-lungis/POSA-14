6602eb1cff4de2c24d670701a5d981cbd9f3792b
Vaidas Lungis <vaidas.lungis@gmail.com>
2014-07-14 08:11:31 +0300

    W8 A7 done

--- assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadBoundServiceAsync.java
+++ assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadBoundServiceAsync.java
@@ -52,6 +52,8 @@ public class DownloadBoundServiceAsync extends Service{
                 // the appropriate helper method in DownloadUtils and
                 // then send the pathname back to the client via the
                 // callback object.
+            	String pathname = DownloadUtils.downloadFile(DownloadBoundServiceAsync.this, uri);
+            	callback.sendPath(pathname);
             }
 		
 	};
@@ -75,6 +77,6 @@ public class DownloadBoundServiceAsync extends Service{
     public static Intent makeIntent(Context context) {
         // TODO - replace the null to create the appropriate Intent
         // and return it to the caller.
-        return null;
+        return new Intent (context, DownloadBoundServiceAsync.class);
     }
 }
